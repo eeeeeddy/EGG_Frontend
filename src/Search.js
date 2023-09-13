f11edaafbe2c3272b4baca5d0cbbf39f718cf7a3
Eddy <marill94727@naver.com>
2023-09-13 23:59:37 +0900

    Search.js 들여쓰기 수정

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,5 @@
 import './Search.css';
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
@@ -10,44 +10,44 @@ function Search() {
 	const navigate = useNavigate();
 
 	const [searchQuery, setSearchQuery] = useState('');
-    const onSubmit = async () => {
-        window.location.href = "/search/" + searchQuery;
-    };
-    const handleKeyDown = (event) => {
-        if (event.key === 'Enter') {
-            if (searchQuery.trim() === '') {
-                window.alert('검색어를 입력하세요.'); 
-            } else {
-            console.log('검색어가 입력되었습니다.');
-            navigate(`/search/${encodeURIComponent(searchQuery)}`);
-            // 검색어를 포함하여 Search 페이지로 이동합니다.
-            }
-        }
-    };
+	const onSubmit = async () => {
+		window.location.href = "/search/" + searchQuery;
+	};
+	const handleKeyDown = (event) => {
+		if (event.key === 'Enter') {
+			if (searchQuery.trim() === '') {
+				window.alert('검색어를 입력하세요.');
+			} else {
+				console.log('검색어가 입력되었습니다.');
+				navigate(`/search/${encodeURIComponent(searchQuery)}`);
+				// 검색어를 포함하여 Search 페이지로 이동합니다.
+			}
+		}
+	};
 
 	useEffect(() => {
 		// URL 파라미터로부터 검색어를 가져옵니다.
 		const { searchQuery } = params;
-	
+
 		// Spring Boot API 엔드포인트에 GET 요청을 보냅니다.
 		axios.get(`/search/?searchKeyword=${searchQuery}`)
-		.then((response) => {
-			// API 응답으로 받은 데이터를 검색 결과로 설정합니다.
-			setSearchResult(response.data);
-			console.log(response.data);
-		})
-		.catch((error) => {
-			console.error('API 요청 중 오류 발생:', error);
-		});
+			.then((response) => {
+				// API 응답으로 받은 데이터를 검색 결과로 설정합니다.
+				setSearchResult(response.data);
+				console.log(response.data);
+			})
+			.catch((error) => {
+				console.error('API 요청 중 오류 발생:', error);
+			});
 	}, [setSearchResult, params]);
 
 	const TextStyle = {
 		textAlign: 'center'
 	};
-	 // paper-box를 클릭했을 때 detail 페이지로 이동하는 함수
-	 const handlePaperBoxClick = (articleId) => {
-        navigate(`/Detail/${articleId}`);
-    };
+	// paper-box를 클릭했을 때 detail 페이지로 이동하는 함수
+	const handlePaperBoxClick = (articleId) => {
+		navigate(`/Detail/${articleId}`);
+	};
 
 	return (
 		<div>
@@ -63,11 +63,11 @@ function Search() {
 							<img src='/search_icon.png' alr='돋보기 아이콘' className='search-icon' />
 
 							<input className='search-input' type='search'
-                            	autoComplete='off' aria-expanded='false'
-                            	aria-live='polite'
-                           		placeholder='검색어를 입력하세요'
+								autoComplete='off' aria-expanded='false'
+								aria-live='polite'
+								placeholder='검색어를 입력하세요'
 								onChange={(e) => setSearchQuery(e.target.value)}
-                            	onKeyDown={handleKeyDown} // 엔터 키 이벤트 처리
+								onKeyDown={handleKeyDown} // 엔터 키 이벤트 처리
 							/>
 						</div>
 					</div>
@@ -77,24 +77,24 @@ function Search() {
 			<div className="paper-container">
 				<div className="paper-text" style={{ float: 'left' }}>
 					<p style={TextStyle}>
-					{params.searchQuery
-                            ? `'${decodeURIComponent(params.searchQuery)}' 에 대한 논문 검색 결과`
-                            : '에 대한 논문 검색 결과'}
-                    </p>
+						{params.searchQuery
+							? `'${decodeURIComponent(params.searchQuery)}' 에 대한 논문 검색 결과`
+							: '에 대한 논문 검색 결과'}
+					</p>
 					<b><h3 style={TextStyle}>Choose Article for Graph : </h3></b>
 					<br />
 				</div>
 				{searchResult.map((result) => (
-                    // 검색 결과를 여기서 필요한대로 렌더링하세요.
-                    <div key={result.article_id}
+					// 검색 결과를 여기서 필요한대로 렌더링하세요.
+					<div key={result.article_id}
 						className="paper-box"
 						onClick={() => handlePaperBoxClick(result.article_id)}>
-                        <h4>{result.title_ko}</h4>
-                        <p>{result.author_name}<br/>
-                        {result.pub_year}<br/><br/>
-                        <span className='paperbox-p'>{result.abstract_ko}</span></p>
-                    </div>
-                ))}
+						<h4>{result.title_ko}</h4>
+						<p>{result.author_name}<br />
+							{result.pub_year}<br /><br />
+							<span className='paperbox-p'>{result.abstract_ko}</span></p>
+					</div>
+				))}
 			</div>
 		</div>
 	);
