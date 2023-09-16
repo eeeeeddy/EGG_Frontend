f77f3aeffb470c5c4d6db11a068c0908bf38a28c
Eddy <marill94727@naver.com>
2023-09-16 15:22:03 +0900

    test 수정

--- src/test.js
+++ src/test.js
@@ -2,7 +2,7 @@ import './Test.css';
 import React, { useState, useEffect } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { Collapse } from 'bootstrap';
+
 
 function Test() {
     const [detailResult, setDetailResult] = useState([]);
