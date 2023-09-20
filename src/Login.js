30904da1108df04f44f6d7a080e1a2f55dbb6107
Eddy <marill94727@naver.com>
2023-09-21 00:20:54 +0900

    Login, SignUp   - 들여쓰기 수정

--- src/Login.js
+++ src/Login.js
@@ -4,118 +4,118 @@ import { Form, Row, Col, Container, Button } from 'react-bootstrap';
 import SignUp from './SignUp';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isEmailValid, setIsEmailValid] = useState(false);
-  const [isRegistering, setIsRegistering] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isEmailValid, setIsEmailValid] = useState(false);
+    const [isRegistering, setIsRegistering] = useState(false);
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
 
-  const handleContinueClick = () => {
-    // 이메일이 회원 목록에 있는지 확인 (시뮬레이션)
-    const isEmailRegistered = checkEmailInServer(email);
+    const handleContinueClick = () => {
+        // 이메일이 회원 목록에 있는지 확인 (시뮬레이션)
+        const isEmailRegistered = checkEmailInServer(email);
 
-    if (isEmailRegistered) {
-      setIsEmailValid(true); // 이메일이 유효하면 상태를 true로 설정
-    } else {
-      setIsRegistering(true); // 회원가입 양식을 보여줌
-    }
-  };
+        if (isEmailRegistered) {
+            setIsEmailValid(true); // 이메일이 유효하면 상태를 true로 설정
+        } else {
+            setIsRegistering(true); // 회원가입 양식을 보여줌
+        }
+    };
 
-  const checkEmailInServer = (email) => {
-    // 이 부분에서 서버와 통신하여 이메일 확인 (시뮬레이션)
-    return false; // 가상으로 true 반환 (시뮬레이션)
-  };
+    const checkEmailInServer = (email) => {
+        // 이 부분에서 서버와 통신하여 이메일 확인 (시뮬레이션)
+        return false; // 가상으로 true 반환 (시뮬레이션)
+    };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
 
-  const handleLoginClick = () => {
-    // 패스워드를 사용하여 로그인
-    console.log('Login in with email:', email, 'password:', password);
+    const handleLoginClick = () => {
+        // 패스워드를 사용하여 로그인
+        console.log('Login in with email:', email, 'password:', password);
 
-    // 이후 로그인 성공 또는 실패 처리를 진행할 수 있습니다.
-  };
+        // 이후 로그인 성공 또는 실패 처리를 진행할 수 있습니다.
+    };
 
-  const handleEmailKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault(); // Enter 키의 기본 동작(새 줄 추가)을 방지합니다.
-      handleContinueClick(); // "Continue with Email" 버튼을 클릭합니다.
-    }
-  };
+    const handleEmailKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault(); // Enter 키의 기본 동작(새 줄 추가)을 방지합니다.
+            handleContinueClick(); // "Continue with Email" 버튼을 클릭합니다.
+        }
+    };
 
-  return (
-    <Container className="panel">
-      <Form>
-        <br />
-        {!isRegistering ? ( // 회원가입 양식이 아닐 때만 이메일 입력란을 표시
-          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-            <Col sm>
-              <Form.Control
-                type="email"
-                placeholder="E-mail"
-                onChange={handleEmailChange}
-                onKeyPress={handleEmailKeyPress} // Enter 키 핸들링 추가
-                value={email}
-              />
-            </Col>
-          </Form.Group>
-        ) : null}
-        {isEmailValid ? (
-          <div>
-            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-              <Col sm>
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  onChange={handlePasswordChange}
-                  value={password}
-                />
-              </Col>
-            </Form.Group>
-            <br />
-            <div className="d-grid gap-1">
-              <Button
-                variant="secondary"
-                type="submit"
-                onClick={handleLoginClick}
-              >
-                Login
-              </Button>
-            </div>
-          </div>
-        ) : (
-          <div>
-            {isRegistering ? (
-              // 회원가입 양식을 보여줄 경우
-              <div>
-                <p style={{ color: 'gray' }}>
-                  This email is not registered.<br />
-                  Please proceed with membership registration.
-                </p>
-                <SignUp email={email} /> {/* 이메일 prop 전달 */}
-              </div>
-            ) : (
-              <div>
+    return (
+        <Container className="panel">
+            <Form>
                 <br />
-                <div className="d-grid gap-1">
-                  <Button
-                    variant="secondary"
-                    type="button"
-                    onClick={handleContinueClick}
-                  >
-                    Continue with Email
-                  </Button>
-                </div>
-              </div>
-            )}
-          </div>
-        )}
-      </Form>
-    </Container>
-  );
+                {!isRegistering ? ( // 회원가입 양식이 아닐 때만 이메일 입력란을 표시
+                    <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                        <Col sm>
+                            <Form.Control
+                                type="email"
+                                placeholder="E-mail"
+                                onChange={handleEmailChange}
+                                onKeyPress={handleEmailKeyPress} // Enter 키 핸들링 추가
+                                value={email}
+                            />
+                        </Col>
+                    </Form.Group>
+                ) : null}
+                {isEmailValid ? (
+                    <div>
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                            <Col sm>
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Password"
+                                    onChange={handlePasswordChange}
+                                    value={password}
+                                />
+                            </Col>
+                        </Form.Group>
+                        <br />
+                        <div className="d-grid gap-1">
+                            <Button
+                                variant="secondary"
+                                type="submit"
+                                onClick={handleLoginClick}
+                            >
+                                Login
+                            </Button>
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        {isRegistering ? (
+                            // 회원가입 양식을 보여줄 경우
+                            <div>
+                                <p style={{ color: 'gray' }}>
+                                    This email is not registered.<br />
+                                    Please proceed with membership registration.
+                                </p>
+                                <SignUp email={email} /> {/* 이메일 prop 전달 */}
+                            </div>
+                        ) : (
+                            <div>
+                                <br />
+                                <div className="d-grid gap-1">
+                                    <Button
+                                        variant="secondary"
+                                        type="button"
+                                        onClick={handleContinueClick}
+                                    >
+                                        Continue with Email
+                                    </Button>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </Form>
+        </Container>
+    );
 }
 export default Login;
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,104 +1,104 @@
-import React, { useState, useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 function SignUp({ email: initialEmail }) {
-  const [email, setEmail] = useState(initialEmail);
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [birthDate, setBirthDate] = useState(null);
-  const [gender, setGender] = useState('');
+    const [email, setEmail] = useState(initialEmail);
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [birthDate, setBirthDate] = useState(null);
+    const [gender, setGender] = useState('');
 
-  useEffect(() => {
-    // 이메일 prop이 변경될 때마다 이메일 입력란에 값을 설정
-    if (initialEmail) {
-      setEmail(initialEmail); // 초기 이메일 값을 설정할 때도 상태 변수 이름 사용
-    }
-  }, [initialEmail]);
+    useEffect(() => {
+        // 이메일 prop이 변경될 때마다 이메일 입력란에 값을 설정
+        if (initialEmail) {
+            setEmail(initialEmail); // 초기 이메일 값을 설정할 때도 상태 변수 이름 사용
+        }
+    }, [initialEmail]);
 
-  const handleSignUp = () => {
-    // 회원가입 로직을 여기에 추가하세요
-    console.log('Signing up with email:', email);
-    console.log('Name:', name);
-    console.log('Password:', password);
-    console.log('Confirm Password:', confirmPassword);
-    console.log('Birth Date:', birthDate);
-    console.log('Gender:', gender);
-  };
+    const handleSignUp = () => {
+        // 회원가입 로직을 여기에 추가하세요
+        console.log('Signing up with email:', email);
+        console.log('Name:', name);
+        console.log('Password:', password);
+        console.log('Confirm Password:', confirmPassword);
+        console.log('Birth Date:', birthDate);
+        console.log('Gender:', gender);
+    };
 
-  return (
-    <Form>
-      <Form.Group controlId="email">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </Form.Group>
+    return (
+        <Form>
+            <Form.Group controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </Form.Group>
 
-      <Form.Group controlId="name">
-        <Form.Label>Name</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </Form.Group>
+            <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </Form.Group>
 
-      <Form.Group controlId="password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </Form.Group>
+            <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </Form.Group>
 
-      <Form.Group controlId="confirmPassword">
-        <Form.Label>Confirm Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Confirm password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </Form.Group>
+            <Form.Group controlId="confirmPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    placeholder="Confirm password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                />
+            </Form.Group>
 
-      <Form.Group controlId="birthDate">
-        <Form.Label>Birth Date</Form.Label>
-        <DatePicker
-          selected={birthDate}
-          onChange={(date) => setBirthDate(date)}
-          dateFormat="MM/dd/yyyy"
-          placeholderText="Select birth date"
-        />
-      </Form.Group>
+            <Form.Group controlId="birthDate">
+                <Form.Label>Birth Date</Form.Label>
+                <DatePicker
+                    selected={birthDate}
+                    onChange={(date) => setBirthDate(date)}
+                    dateFormat="MM/dd/yyyy"
+                    placeholderText="Select birth date"
+                />
+            </Form.Group>
 
-      <Form.Group controlId="gender">
-        <Form.Label>Gender</Form.Label>
-        <Form.Control
-          as="select"
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
-        >
-          <option value="">Select gender</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </Form.Control>
-      </Form.Group>
+            <Form.Group controlId="gender">
+                <Form.Label>Gender</Form.Label>
+                <Form.Control
+                    as="select"
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                >
+                    <option value="">Select gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </Form.Control>
+            </Form.Group>
 
-      <Button variant="primary" onClick={handleSignUp}>
-        Sign Up
-      </Button>
-    </Form>
-  );
+            <Button variant="primary" onClick={handleSignUp}>
+                Sign Up
+            </Button>
+        </Form>
+    );
 }
 
 export default SignUp;
