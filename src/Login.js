c7c456a2eee41b1a38f8491259e0cefc77614e5d
ay30n <suhyeon591@gmail.com>
2023-09-21 17:47:09 +0900

    로그인서버 연결_ver1 - sh

--- src/Login.js
+++ src/Login.js
@@ -2,50 +2,66 @@
 import React, { useState } from 'react';
 import { Form, Row, Col, Container, Button } from 'react-bootstrap';
 import SignUp from './SignUp';
+import axios from 'axios';
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [isEmailValid, setIsEmailValid] = useState(false);
-    const [isRegistering, setIsRegistering] = useState(false);
+    const [isEmailValid, setIsEmailValid] = useState('');
+    const [isRegistering, setIsRegistering] = useState('');
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
     };
 
-    const handleContinueClick = () => {
-        // 이메일이 회원 목록에 있는지 확인 (시뮬레이션)
-        const isEmailRegistered = checkEmailInServer(email);
-
-        if (isEmailRegistered) {
-            setIsEmailValid(true); // 이메일이 유효하면 상태를 true로 설정
-        } else {
-            setIsRegistering(true); // 회원가입 양식을 보여줌
+    const handleContinueClick = async () => {
+        try {
+            // 이메일이 서버에 있는지 확인
+            const response = await axios.get(`/users/checkEmail?email=${email}`);
+            console.log(response)
+            if (response.data.result.exists == true) {
+                setIsEmailValid(true); // 이메일이 유효하면 상태를 true로 설정
+                setIsRegistering(false); // 이미 등록된 이메일이므로 회원가입 화면이 아닌 로그인 화면을 보여줌
+            } else {
+                setIsEmailValid(false); // 이메일이 유효하지 않으면 상태를 false로 설정
+                setIsRegistering(true); // 회원가입 양식을 보여줌
+            }
+        } catch (error) {
+            console.error('Error checking email', error);
         }
     };
-
-    const checkEmailInServer = (email) => {
-        // 이 부분에서 서버와 통신하여 이메일 확인 (시뮬레이션)
-        return false; // 가상으로 true 반환 (시뮬레이션)
-    };
-
+    
+    
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
     };
 
-    const handleLoginClick = () => {
-        // 패스워드를 사용하여 로그인
-        console.log('Login in with email:', email, 'password:', password);
+    const handleLoginClick = async () => {
+        try {
+            // 서버로 로그인 요청을 보냄
+            const response = await axios.post('/users/login', {
+                email,
+                password,
+            });
 
-        // 이후 로그인 성공 또는 실패 처리를 진행할 수 있습니다.
-    };
+            // 로그인이 성공하면 토큰을 받아올 수 있음
+            const token = response.data;
 
-    const handleEmailKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            e.preventDefault(); // Enter 키의 기본 동작(새 줄 추가)을 방지합니다.
-            handleContinueClick(); // "Continue with Email" 버튼을 클릭합니다.
+            // 이후 토큰을 사용하여 로그인 상태를 관리하거나 보호된 리소스에 접근 가능
+            console.log('Login successful. Token:', token);
+
+            // 로그인 후의 추가 작업을 수행하거나 리디렉션을 할 수 있음
+        } catch (error) {
+            console.error('Error logging in', error);
         }
     };
+
+    // const handleEmailKeyPress = (e) => {
+    //     if (e.key === 'Enter') {
+    //         e.preventDefault(); // Enter 키의 기본 동작(새 줄 추가)을 방지합니다.
+    //         handleContinueClick(); // "Continue with Email" 버튼을 클릭합니다.
+    //     }
+    // };
 
     return (
         <Container className="panel">
@@ -58,7 +74,6 @@ function Login() {
                                 type="email"
                                 placeholder="E-mail"
                                 onChange={handleEmailChange}
-                                onKeyPress={handleEmailKeyPress} // Enter 키 핸들링 추가
                                 value={email}
                             />
                         </Col>
--- src/MainNavbar.js
+++ src/MainNavbar.js
@@ -21,24 +21,24 @@ function EggNavbar() {
 
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/">
-                        <img src="/ditto_logo.jpg" alt="" width="32" height="32" class="d-inline-block align-text-top" />
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        <img src="/ditto_logo.jpg" alt="" width="32" height="32" className="d-inline-block align-text-top" />
                     </a>
-                    <a class="navbar-brand" href="/">EGG</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <a className="navbar-brand" href="/">EGG</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                             <Nav>
                                 {/* <Nav.Link style={{ color: 'black' }} href="/About">About</Nav.Link>
                                 <Nav.Link style={{ color: 'black' }} href="/Pricing">Pricing</Nav.Link> */}
                                 <Nav.Link style={{ color: 'black' }} onClick={handleLoginClick} >
-                                    <svg width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                    <svg width="24" height="24" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                                        <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                                     </svg>
                                 </Nav.Link>
                             </Nav>
