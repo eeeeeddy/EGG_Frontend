6260c56f85887024a1b951a4f228aab08a72ab96
eeeeeddy <marill94727@naver.com>
2023-10-10 11:17:34 +0900

    ElasticSearch 주소 수정

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import EggNavbar from './Navbar';
 
 const Dashboard = () => {
-    const kibanaDashboardURL = 'http://43.201.21.9:5601/app/dashboards#/view/cb9afad0-6681-11ee-9d67-957404d3de86?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))';
+    const kibanaDashboardURL = 'http://3.35.150.101:5601/app/dashboards#/view/cb9afad0-6681-11ee-9d67-957404d3de86?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))&show-top-menu=true&show-query-input=true&show-time-filter=true';
 
     return (
         <div>
